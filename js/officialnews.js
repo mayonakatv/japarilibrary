f3c772d67f1737abe095453e04d045dbb807d7e6
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-08-08 23:26:37 +0900

    [Mod] index
    微修正

--- js/officialnews.js
+++ js/officialnews.js
@@ -25,7 +25,8 @@ var officialnews = new Vue({
         { id: 4, href: 'https://twitter.com/kemono_matsuri/status/1022739201903616002', seen: true, remark: '<br /><span class="red-text">※返金方法は下記記載あり</span>', text: 'こんばんは〜！ 【7月28日（土）の返金手続き】 返金方法につきまして、記載しております楽天チケットＵＲＬよりご確認いただき、対象のチケットをお持ちの方は、期間中にお手続きをお願い致します。 （※対象外のお客様への返金はいたしかねますのでご了承ください）<a href="https://ticket.rakuten.co.jp/change" rel="external" title="return">https://ticket.rakuten.co.jp/change</a> #けもフレがーでん' },
         { id: 5, href: 'https://twitter.com/kemono_matsuri/status/1023419590917779456', seen: false, remark: defaultValue.remark, text: 'みなさーん がーでんジョッキ(ppp集合ver)ですが、完売致しました！！ 入荷次第Twitterにあげますので、要チェックでお願いしまーす！ - 2018年7月24日 13:06' },
         { id: 6, href: 'https://twitter.com/kemono_matsuri/status/1024561148840960000', seen: true, remark: defaultValue.remark, text: '皆さーん そして今日からなんとスタンプラリーのスタンプキャラが変わるのだー！！ 果たして誰になるのか、、、 スタンプも要チェックなのだー！！ - 2018年8月1日 16:43' },
-        { id: 7, href: 'https://twitter.com/kemono_matsuri/status/1024928621704564736', seen: true, remark: defaultValue.remark, text: '皆さーん 今日も暑いけどみんなのこと待ってるのだー そして今日から去年も販売していたジャンボうちわを販売しまーす！！パチパチ このうちわで今年の夏も乗り切るのだー - 2018年8月2日 17:03' }
+        { id: 7, href: 'https://twitter.com/kemono_matsuri/status/1024928621704564736', seen: true, remark: defaultValue.remark, text: '皆さーん 今日も暑いけどみんなのこと待ってるのだー そして今日から去年も販売していたジャンボうちわを販売しまーす！！パチパチ このうちわで今年の夏も乗り切るのだー - 2018年8月2日 17:03' },
+        { id: 8, href: 'https://twitter.com/kemono_matsuri/status/1027032997151760385', seen: true, remark: defaultValue.remark, text: '【営業中止のお知らせ】本日8/8(水)の営業ですが、台風の接近に伴い終日営業中止とさせて頂きます。ご来店予定だった皆様、大変申し訳ございません。またのご来店心よりお待ちしております。 #けもフレがーでん - 12:25 - 2018年8月8日' }
       ]
     }
   }
