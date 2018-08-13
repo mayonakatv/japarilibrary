b5f9ac39a6949b485c0e2d0fb7996d4945b5f196
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-08-13 10:54:53 +0900

    [Mod] index
    モーダル差し戻しとGoodsにトップへ戻るボタン

--- js/myScript.js
+++ js/myScript.js
@@ -1,31 +1,5 @@
 //http://exrecord.net/how-to-create-overlay-and-modal-window-in-jquery
-$(function () {
-  $('#overlay, .modalWindow._1').fadeIn();
-
-  $('.closeBtn_1').on('click tap', function () {
-    $('#overlay, .modalWindow').fadeOut();
-    $('#overlay, .modalWindow._2').fadeIn();
-  });
-  $('.closeBtn').on('click tap', function () {
-    $('#overlay, .modalWindow').fadeOut();
-  });
-
-  locateCenter();
-  $(window).resize(locateCenter);
-
-  function locateCenter() {
-    let w = $(window).width();
-    let h = $(window).height();
-
-    let cw = $('.modalWindow').outerWidth();
-    let ch = $('.modalWindow').outerHeight();
-
-    $('.modalWindow').css({
-      'left': ((w - cw) / 2) + 'px',
-      'top': ((h - ch) / 3) + 'px'
-    });
-  }
-});
+//deleted
 
 $(function () {
   flag = 0;
