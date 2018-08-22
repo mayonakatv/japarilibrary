70dcb4c5ae54fac450cb01f0210b9eaa9b4f2a16
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-08-23 01:23:42 +0900

    [Add] siteInfo
    サイト情報分割

---
+++ siteInfo.php
@@ -0,0 +1,14 @@
+<?php
+require 'header.php';
+
+require 'contents/siteInfo.html';
+require 'contents/specialthanks.html';
+?>
+
+<div class="pagenation">
+  <a href="/"  rel="nofollow" class="btn btn-large">
+    <i class="fas fa-home"></i> トップページへ戻る
+  </a>
+</div>
+
+<?php require 'footer.php';
