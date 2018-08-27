bf6ccbdb198051318fbee4b6c073c9c14c6d7549
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-08-28 04:40:20 +0900

    [Mod] index
    微修正

--- index.php
+++ index.php
@@ -1,51 +1,52 @@
 <?php
   require 'header.php';
 ?>
-    <!-- <div id="carousel" class="carousel carousel-slider hide-on-med-and-down">
-      <carousel-item v-for="item in carouselGroceryList" v-bind:todo="item" v-bind:target="carousel"></carousel-item>
-    </div> -->
-    <div class="cyclotron cycle hide-on-med-and-down" style="background:url(img/panorama/panorama_<?php echo mt_rand(1,2); ?>.jpg);height:512px"></div>
+  <!-- <div id="carousel" class="carousel carousel-slider hide-on-med-and-down">
+    <carousel-item v-for="item in carouselGroceryList" v-bind:todo="item" v-bind:target="carousel"></carousel-item>
+  </div> -->
+  <div class="cyclotron cycle hide-on-med-and-down" style="background:url(img/panorama/panorama_<?php echo mt_rand(1,2); ?>.jpg);height:512px"></div>
+
+  <?php
+    require 'contents/sect-about.html';
+    require 'contents/sect-officialNews.html';
+  ?>
+
+  <div class="addLazyloadCss">
 
     <?php
-      require 'contents/sect-about.html';
-      require 'contents/sect-officialNews.html';
+      require 'contents/sect-map.html';
+      require 'contents/sect-faq.html'
     ?>
 
-    <div class="addLazyloadCss">
+    <section id="sect-first">
+      <h2>初めての方へ / 楽しみ方のご提案</h2>
+      <p>そもそも「けものフレンズ」って何？と言う方は
+        <a href="firstEntry.php" title="初参加者の方へ！">こちらから！</a>
+        <br />きっとあなたも好きになる！
+      </p>
+    </section>
+
+    <?php
+      require 'contents/sect-numberedTickets.html';
+      require 'contents/sect-100.html';
+    ?>
+
+    <div id="gardengoods">
 
       <?php
-        require 'contents/sect-map.html';
-        require 'contents/sect-faq.html'
-      ?>
-
-      <section id="sect-first">
-        <h2>初めての方へ / 楽しみ方のご提案</h2>
-        <p>別ページをご用意いたしました！
-          <a href="firstEntry.php" title="初参加者の方へ！">こちらからご確認ください！</a>
-        </p>
-      </section>
-
-      <?php
-        require 'contents/sect-numberedTickets.html';
-        require 'contents/sect-100.html';
-      ?>
-
-      <div id="gardengoods">
-
-        <?php
-          require 'contents/sect-gardengoods.html';
-          require 'contents/sect-facility.html'
-        ?>
-
-      </div>
-      <!--[end]gardengoods-->
-
-      <?php
-        require 'contents/sect-notice.html';
-        require 'contents/sect-ads.html'
+        require 'contents/sect-gardengoods.html';
+        require 'contents/sect-facility.html'
       ?>
 
     </div>
-    <!--[end]lazyload-->
+    <!--[end]gardengoods-->
+
+    <?php
+      require 'contents/sect-notice.html';
+      require 'contents/sect-ads.html'
+    ?>
+
+  </div>
+  <!--[end]lazyload-->
 <?php
 require 'footer.php';
