5ce5809ecc1b4b41a408e6bed45430724e20232e
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-09-05 14:05:50 +0900

    [Mod] footer
    一部非表示と非同期実行

--- footer.php
+++ footer.php
@@ -16,10 +16,10 @@
 
   <script type="text/javascript" src="js/dropdown.js"></script>
   <script type="text/javascript" src="js/officialnews.js"></script>
-  <script type="text/javascript" src="js/carousel.js"></script>
-  <script type="text/javascript" src="js/gardengoods.js"></script>
-  <script type="text/javascript" src="js/notice.js"></script>
-  <script type="text/javascript" src="js/spthx.js"></script>
+  <!-- <script type="text/javascript" src="js/carousel.js" defer></script> -->
+  <script type="text/javascript" src="js/gardengoods.js" defer></script>
+  <script type="text/javascript" src="js/notice.js" defer></script>
+  <script type="text/javascript" src="js/spthx.js" defer></script>
 
   <!--代替画像表示_一番最後に実行のこと！-->
   <script type="text/javascript" src="js/alternateImage.js"></script>
