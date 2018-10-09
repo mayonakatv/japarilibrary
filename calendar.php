9486a2eaa4f48beeff5984112fde3551b4371c08
MayonakaDarjeeling <nao@naotonoMacBook-Pro.local>
2018-10-09 16:47:21 +0900

    [Mod] cal
    表題追記

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <?php
   require 'header.php';
 ?>
-  <h1>がーでん整理券情報カレンダー</h1>
+  
   <div class="addLazyloadCss">
     <iframe src="img/sysimg/bossLoading.jpg" data-src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;hl=ja&amp;bgcolor=%23ffffff&amp;src=mayonaka.tv_j6qgcishtkembmh53sr3pq10sk%40group.calendar.google.com&amp;color=%235F6B02&amp;ctz=Asia%2FTokyo" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
   </div>
